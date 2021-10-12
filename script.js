db46f5527d8bbc171bd5eb60c42898d7eb2c7151
Rayed Khan <60972839+racray@users.noreply.github.com>
2021-10-12 20:12:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(()=>{
+    document.querySelector("h1").innerText = "10";
+    setTimeout(()=>{
+        document.querySelector("h1").innerText = "9";
+        setTimeout(()=>{
+            document.querySelector("h1").innerText = "8";
+            setTimeout(()=>{
+                document.querySelector("h1").innerText = "7";
+                setTimeout(()=>{
+                    document.querySelector("h1").innerText = "6";
+                    setTimeout(()=>{
+                        document.querySelector("h1").innerText = "5";
+                        setTimeout(()=>{
+                            document.querySelector("h1").innerText = "4";
+                            setTimeout(()=>{
+                                document.querySelector("h1").innerText = "3";
+                                setTimeout(()=>{
+                                    document.querySelector("h1").innerText = "2";
+                                    setTimeout(()=>{
+                                        document.querySelector("h1").innerText = "1";
+                                        setTimeout(()=>{
+                                            document.querySelector("h1").innerText = "Happy Independence Day";
+                                            document.querySelector("body").style.backgroundColor = "orange";
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
